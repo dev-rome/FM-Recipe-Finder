03cbaab4a661c0afa24b83ce9597b836306b2b34
dev-rome <info.devrome@gmail.com>
2025-11-16 16:48:03 -0500

    refactor: update SectionDivider to show on all devices with consistent spacing and remove unused props

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,11 @@
 import Mission from "@/sections/About/Mission";
+import SectionDivider from "@/components/section-divider/SectionDivider";
 
 export default function About() {
   return (
     <>
       <Mission />
+      <SectionDivider />
     </>
   );
 }
--- components/section-divider/SectionDivider.tsx
+++ components/section-divider/SectionDivider.tsx
@@ -1,32 +1,7 @@
 import styles from "./SectionDivider.module.css";
 
-interface SectionDividerProps {
-  marginTop?: string;
-  marginBottom?: string;
-  className?: string;
-}
-
-const SectionDivider = ({
-  marginTop,
-  marginBottom,
-  className = "",
-}: SectionDividerProps) => {
-  const dividerClasses = [styles.sectionDivider, className]
-    .filter(Boolean)
-    .join(" ");
-
-  const customStyles: React.CSSProperties = {};
-
-  if (marginTop) customStyles.marginTop = marginTop;
-
-  if (marginBottom) customStyles.marginBottom = marginBottom;
-
-  return (
-    <hr
-      className={dividerClasses}
-      style={Object.keys(customStyles).length > 0 ? customStyles : undefined}
-    />
-  );
+const SectionDivider = () => {
+  return <hr className={styles.sectionDivider} />;
 };
 
 export default SectionDivider;
